d5e6dda3fa63357742ef983f2bc02e3b72a82258
Wolfgang Richter <wolf@cs.cmu.edu>
2010-12-13 18:43:02 -0500

    Code cleanup, license details, stylized benchmark.c.
    Prepping for GPLv2 release.

--- benchmark.c
+++ benchmark.c
@@ -1,3 +1,28 @@
+/******************************************************************************
+ * benchmark.c                                                                *
+ *                                                                            *
+ * This is an application that can benchmark libperf performance using the    *
+ * sysbench benchmarking tool.                                                *
+ *                                                                            *
+ * libperf interfaces with the kernel performance counters subsystem          *
+ * Copyright (C) 2010 Wolfgang Richter, Ekaterina Taralova, Karl Naden        *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or              *
+ * modify it under the terms of the GNU General Public License                *
+ * as published by the Free Software Foundation; either version 2             *
+ * of the License, or (at your option) any later version.                     *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program; if not, write to the Free Software                *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA              *
+ * 02110-1301, USA.                                                           *
+ ******************************************************************************/
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -5,39 +30,45 @@
 #include <unistd.h>
 #include "libperf.h"
 
-char* const tests[][10] = {
-	{"/usr/bin/sysbench","--test=cpu","--cpu-max-prime=20000", "run", (char*) 0},
-	{"/usr/bin/sysbench","--num-threads=64","--test=threads", "--thread-yields=7500", "--thread-locks=8", "run", (char*) 0},
-	{"/usr/bin/sysbench","--test=mutex", "--mutex-num=4096","--mutex-locks=300000000", "--mutex-loops=10000000", "run", (char*) 0},
-	{"/usr/bin/sysbench","--test=memory","--memory-block-size=4K","--memory-total-size=23G","run", (char*) 0},
-	{"/usr/bin/sysbench", "--num-threads=16", "--test=fileio", "--file-total-size=3G", "--file-test-mode=rndrw", "prepare", (char*) 0},
-	{"/usr/bin/sysbench", "--num-threads=2", "--max-requests=7000", "--test=fileio", "--file-total-size=3G","--file-test-mode=rndrw", "run", (char*) 0},
-	{"/usr/bin/sysbench", "--num-threads=16", "--test=fileio", "--file-total-size=3G", "--file-test-mode=rndrw", "clean", (char*) 0}
+char *const tests[][10] = {
+  {"/usr/bin/sysbench", "--test=cpu", "--cpu-max-prime=20000", "run", (char *) 0},
+  {"/usr/bin/sysbench", "--num-threads=64", "--test=threads", "--thread-yields=7500", "--thread-locks=8", "run", (char *) 0},
+  {"/usr/bin/sysbench", "--test=mutex", "--mutex-num=4096", "--mutex-locks=300000000", "--mutex-loops=10000000", "run", (char *) 0},
+  {"/usr/bin/sysbench", "--test=memory", "--memory-block-size=4K", "--memory-total-size=23G", "run", (char *) 0},
+  {"/usr/bin/sysbench", "--num-threads=16", "--test=fileio", "--file-total-size=3G", "--file-test-mode=rndrw", "prepare", (char *) 0},
+  {"/usr/bin/sysbench", "--num-threads=2", "--max-requests=7000", "--test=fileio", "--file-total-size=3G", "--file-test-mode=rndrw", "run", (char *) 0},
+  {"/usr/bin/sysbench", "--num-threads=16", "--test=fileio", "--file-total-size=3G", "--file-test-mode=rndrw", "clean", (char *) 0}
 };
 
-int main(int argc, char* argv[])
+int
+main(int argc, char *argv[])
 {
-	if (argc < 2)
-		fprintf(stderr, "Usage: bench <test #>\n");
-	int test = atoi(argv[1]), status, i = 0;
-	
-	fprintf(stdout, "cmd[");
-	while (tests[test][i] != 0) fprintf(stdout, " %s", tests[test][i++]);
-	fprintf(stdout, " ]\n");
+  if (argc < 2)
+    fprintf(stderr, "Usage: bench <test #>\n");
 
-	
-	pid_t pid = fork();
+  int test = atoi(argv[1]), status, i = 0;
 
-	struct perf_data* pd = libperf_initialize(pid,-1);
+  fprintf(stdout, "cmd[");
 
-	if (pid == 0)
-	{
-		execv(tests[test][0], tests[test]);
-	}
-	else
-	{
-		wait(&status);
-		libperf_finalize(pd, pd);
-	}
-	return 0;
+  while (tests[test][i] != 0)
+    fprintf(stdout, " %s", tests[test][i++]);
+
+  fprintf(stdout, " ]\n");
+
+
+  pid_t pid = fork();
+
+  struct perf_data *pd = libperf_initialize(pid, -1);
+
+  if (pid == 0)
+  {
+      execv(tests[test][0], tests[test]);
+  }
+  else
+  {
+      wait(&status);
+      libperf_finalize(pd, pd);
+  }
+
+  return 0;
 }
---
+++ benchmark.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+###############################################################################
+# benchmark.sh                                                                #
+#                                                                             #
+# This bash script runs a series of benchmarks using the benchmark tool       #
+# (benchmark.c).  It saves the output in a series of log files.               #
+#                                                                             #
+# libperf interfaces with the kernel performance counters subsystem           #
+# Copyright (C) 2010 Wolfgang Richter, Ekaterina Taralova, Karl Naden         #
+#                                                                             #
+# This program is free software; you can redistribute it and/or               #
+# modify it under the terms of the GNU General Public License                 #
+# as published by the Free Software Foundation; either version 2              #
+# of the License, or (at your option) any later version.                      #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program; if not, write to the Free Software                 #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA               #
+# 02110-1301, USA.                                                            #
+###############################################################################
+
+LIB_DIR=/home/wolf/Dropbox/Projects/LightSpeed/src/libperf
+TRIALS="{1..10}"
+
+# perf wrapped
+for i in $TRIALS; do sudo LD_LIBRARY_PATH=$LIB_DIR ./bench 0 >> cpu.libperf.log; done
+for i in $TRIALS; do sudo LD_LIBRARY_PATH=$LIB_DIR ./bench 3 >> mem.libperf.log; done
+for i in $TRIALS; do sudo LD_LIBRARY_PATH=$LIB_DIR ./bench 1  >> threads.libperf.log; done
+for i in $TRIALS; do sudo LD_LIBRARY_PATH=$LIB_DIR ./bench 5  >> IO.libperf.log; done
+for i in $TRIALS; do sudo LD_LIBRARY_PATH=$LIB_DIR ./bench 2  >> mutex.libperf.log; done
+
+# bare
+for i in $TRIALS; do sudo /usr/bin/sysbench --test=cpu --cpu-max-prime=20000 run >> cpu.bare.log; done
+for i in $TRIALS; do sudo /usr/bin/sysbench --test=memory --memory-block-size=4K --memory-total-size=23G run  >> mem.bare.log; done
+for i in $TRIALS; do sudo /usr/bin/sysbench --num-threads=64 --test=threads --thread-yields=7500 --thread-locks=8 run  >> threads.bare.log; done
+for i in $TRIALS; do sudo /usr/bin/sysbench --num-threads=2 --max-requests=7000 --test=fileio --file-total-size=3G --file-test-mode=rndrw run  >> IO.bare.log; done
+for i in $TRIALS; do sudo /usr/bin/sysbench --test=mutex --mutex-num=4096 --mutex-locks=300000000 --mutex-loops=10000000 run  >> mutex.bare.log; done
